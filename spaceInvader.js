4da566ce39588e47e9cafbda49c3ba394adadaec
Olivier Michalski <olivier.michalski@student.ehb.be>
2022-10-11 20:30:22 +0200

    space invader klaar olivier

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,63 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+let canvas = document.querySelector("canvas");
+canvas.width
+= window.innerWidth;
+canvas.height
+= window.innerHeight;
+let context = canvas.getContext('2d' );
+
+drawInvader();
+
+function drawInvader(){
+
+    context.beginPath();
+    context.rect(25 ,25 ,300 ,300 );
+    context.fill()
+
+    
+    context.beginPath();
+    context.fillStyle = "#66FF66"
+    context.rect(50 ,50 ,50 ,50 );
+    context.fill()
+
+
+    context.beginPath();
+    context.rect(100 ,100 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(50 ,100 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(100 ,150 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(150 ,150 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(200 ,150 ,50 ,50 );
+    context.fill()
+
+    context.beginPath();
+    context.rect(200 ,100 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(250 ,100 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(250 ,50 ,50 ,50 );
+    context.fill()
+    
+    context.beginPath();
+    context.rect(150 ,50 ,50 ,50 );
+    context.fill()
+
+
+}
